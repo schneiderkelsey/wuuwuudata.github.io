01398e2bf1540765e3324001d0c485873311a016
Kelsey Schneider <schneiderkelsey000@gmail.com>
2025-12-18 10:28:42 -0500

    updated project visualizations and summary

--- file_creater.py
+++
@@ -1,9 +0,0 @@
-name = input("What's your name? /n")
-
-with open('example.txt', 'w') as file:
-    file.write(f"{name} created this file") 
-    
-color = input("What's your favorite color? /n")
-
-with open('example.txt', 'w') as file:
-    file.write(f"{color} is your favorite color") 
